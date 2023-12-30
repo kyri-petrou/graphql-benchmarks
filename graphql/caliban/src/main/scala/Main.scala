dd24260a88d13cd71c2778bee15f1626deae941f
Kyri Petrou <kyri.petrou@outlook.com>
2023-12-31 10:17:14 +1100

    Use URING as netty's channel

--- graphql/caliban/src/main/scala/Main.scala
+++ graphql/caliban/src/main/scala/Main.scala
@@ -1,12 +1,21 @@
 import caliban.*
 import caliban.quick.*
 import zio.*
+import zio.http.{Client as _, *}
+import zio.http.netty.{ChannelType, NettyConfig}
 
 object Main extends ZIOAppDefault {
   private val api = graphQL(RootResolver(Query(Service.posts)))
 
   def run =
     api
-      .runServer(8000, apiPath = "/graphql")
-      .provide(Service.layer, Client.live)
+      .toApp("/graphql")
+      .flatMap(Server.serve)
+      .provide(
+        Service.layer,
+        Client.live,
+        Server.customized,
+        ZLayer.succeed(Server.Config.default.port(8000)),
+        ZLayer.succeed(NettyConfig.default.channelType(ChannelType.URING))
+      )
 }
