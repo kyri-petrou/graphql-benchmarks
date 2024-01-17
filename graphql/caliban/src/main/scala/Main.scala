5abe94e99fc4fa0fdc4b2bc3740eded988600898
Kyri Petrou <kyri.petrou@outlook.com>
2024-01-17 17:48:46 +1100

    Switch to `URING` channel

--- graphql/caliban/src/main/scala/Main.scala
+++ graphql/caliban/src/main/scala/Main.scala
@@ -2,13 +2,28 @@ import caliban.*
 import caliban.execution.QueryExecution
 import caliban.quick.*
 import zio.*
+import zio.http.Server
+import zio.http.netty.{ChannelType, NettyConfig}
 
 object Main extends ZIOAppDefault {
-  private val api = graphQL(RootResolver(Query(Service.posts)))
+  private val api                                      = graphQL(RootResolver(Query(Service.posts)))
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] = Runtime.removeDefaultLoggers
 
   def run =
     api
-      .runServer(8000, apiPath = "/graphql")
-      .provide(Service.layer, Client.live, ZLayer.scoped(Configurator.setQueryExecution(QueryExecution.Batched)))
+      .toApp(apiPath = "/graphql")
+      .flatMap(Server.serve)
+      .provide(
+        Server.customized,
+        ZLayer.succeed(Server.Config.default.port(8000)),
+        ZLayer.succeed(
+          NettyConfig.defaultWithFastShutdown
+            .maxThreads(java.lang.Runtime.getRuntime.availableProcessors())
+            .leakDetection(NettyConfig.LeakDetectionLevel.DISABLED)
+            .channelType(ChannelType.URING)
+        ),
+        Service.layer,
+        Client.live,
+        ZLayer.scoped(Configurator.setQueryExecution(QueryExecution.Batched))
+      )
 }
