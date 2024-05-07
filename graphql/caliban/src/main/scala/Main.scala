b0192312e1619d1f16d8a6eed7c528ec1a1fa02b
Kyri Petrou <kyri.petrou@outlook.com>
2024-05-07 21:43:57 +1000

    Test io-uring

--- graphql/caliban/src/main/scala/Main.scala
+++ graphql/caliban/src/main/scala/Main.scala
@@ -1,15 +1,27 @@
 import caliban.*
 import caliban.execution.QueryExecution
-import caliban.quick.*
 import zio.*
+import zio.http.netty.{ChannelType, NettyConfig}
+import zio.http.{Client as _, *}
 
 object Main extends ZIOAppDefault {
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] =
-    Runtime.removeDefaultLoggers ++ Runtime.disableFlags(RuntimeFlag.FiberRoots)
+    ZLayer.make[Any](Runtime.removeDefaultLoggers, Runtime.disableFlags(RuntimeFlag.FiberRoots))
 
   private val api = graphQL(RootResolver(Query(Service.posts)))
+
+  private val nProcessors = java.lang.Runtime.getRuntime.availableProcessors()
+
   def run =
-    api
-      .runServer(8000, apiPath = "/graphql")
-      .provide(Service.layer, Client.live, ZLayer.scoped(Configurator.setQueryExecution(QueryExecution.Batched)))
+    Server
+      .serve(QuickAdapter(api.interpreterUnsafe).toApp(apiPath = "/graphql"))
+      .provideSomeEnvironment[Service & Server](_.prune)
+      .provide(
+        Server.customized,
+        ZLayer.succeed(Server.Config.default.port(8000)),
+        ZLayer.succeed(NettyConfig.default.maxThreads(nProcessors).channelType(ChannelType.URING)),
+        Service.layer,
+        Client.live,
+        ZLayer.scoped(Configurator.setQueryExecution(QueryExecution.Batched))
+      )
 }
