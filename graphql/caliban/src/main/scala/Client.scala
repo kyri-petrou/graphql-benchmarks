a0e6e907236e17c93687c43f521a0ff79d14988f
Kyri Petrou <kyri.petrou@outlook.com>
2024-05-22 09:18:45 +0200

    Revert "Revert "Fix body handling""
    This reverts commit d7e50e477a5995aef62b7167910fadad6526c0a0.

--- graphql/caliban/src/main/scala/Client.scala
+++ graphql/caliban/src/main/scala/Client.scala
@@ -1,10 +1,9 @@
 import com.github.plokhotnyuk.jsoniter_scala.core.*
-import jdk.internal.net.http.ResponseSubscribers
 import zio.*
 
 import java.net.*
+import java.net.http.HttpResponse.BodyHandlers
 import java.net.http.{HttpClient, HttpRequest, HttpResponse}
-import java.util.function.Function
 
 trait Client {
   def get[A](url: URI)(using JsonValueCodec[A]): Task[A]
@@ -13,28 +12,22 @@ trait Client {
 object Client {
   private final class Live(client: HttpClient) extends Client {
 
-    private final class ResponseDecoder[A](using JsonValueCodec[A]) extends HttpResponse.BodyHandler[A] {
-      override def apply(responseInfo: HttpResponse.ResponseInfo): HttpResponse.BodySubscriber[A] = {
-        new ResponseSubscribers.ByteArraySubscriber[A](readFromArray[A](_))
-      }
-    }
-
     def get[A](uri: URI)(using JsonValueCodec[A]): Task[A] = {
-      val req = HttpRequest.newBuilder(uri).GET().build()
-      val handler = new ResponseDecoder[A]
-      ZIO.attempt(client.send(req, handler).body())
+      val req     = HttpRequest.newBuilder(uri).GET().build()
+      val handler = BodyHandlers.ofByteArray()
+      ZIO.attempt(readFromArray(client.send(req, handler).body()))
     }
   }
 
   private val httpClient: TaskLayer[HttpClient] = ZLayer.scoped(
     ZIO.fromAutoCloseable(
-      ZIO
-        .executor.map { e =>
-          HttpClient.newBuilder()
-            .proxy(ProxySelector.of(new InetSocketAddress("127.0.0.1", 3000)))
-            .executor(e.asJava)
-            .build()
-        }
+      ZIO.executor.map { e =>
+        HttpClient
+          .newBuilder()
+          .proxy(ProxySelector.of(new InetSocketAddress("127.0.0.1", 3000)))
+          .executor(e.asJava)
+          .build()
+      }
     )
   )
 
