c36cc6e52875142cc7aa860672f922940aad3fec
Kyri Petrou <kyri.petrou@outlook.com>
2024-06-24 10:29:33 +1000

    Call `users` endpoint sequentially

--- graphql/caliban/src/main/scala/Service.scala
+++ graphql/caliban/src/main/scala/Service.scala
@@ -4,14 +4,17 @@ import zio.query.*
 import zio.{Chunk, RIO, RLayer, Task, ZIO, ZLayer}
 
 import java.net.URI
+import scala.collection.mutable
 
 trait Service {
   val posts: Task[List[Post]]
+
   def user(id: Int): TaskQuery[User]
 }
 
 object Service {
-  val posts: RIO[Service, List[Post]]      = ZIO.serviceWithZIO(_.posts)
+  val posts: RIO[Service, List[Post]] = ZIO.serviceWithZIO(_.posts)
+
   def user(id: Int): RQuery[Service, User] = ZQuery.serviceWithQuery(_.user(id))
 
   val layer: RLayer[Client, Service] = ZLayer.derive[Live]
@@ -28,19 +31,19 @@ object Service {
       UsersDataSource.get(id)
 
     private object UsersDataSource {
-      def get(id: Int): TaskQuery[User] = ZQuery.fromRequest(Req(id))(usersDS)
+      def get(id: Int): TaskQuery[User] = usersDS.query(Req(id.toString))
 
-      private case class Req(id: Int) extends Request[Throwable, User] {
-        def toQueryParam: (String, String) = ("id", id.toString)
+      private case class Req(id: String) extends Request[Throwable, User] {
+        def toQueryParam: (String, String) = ("id", id)
       }
 
       private val usersDS = DataSource.fromFunctionBatchedZIO("UsersDataSource") { (reqs: Chunk[Req]) =>
-        client
-          .get[Array[User]](UsersUri, reqs.map(_.toQueryParam))
-          .map(Chunk.fromArray)
+        ZIO.foreach(reqs) { req =>
+          client.get[List[User]](UsersUri, Chunk.single(req.toQueryParam)).map(_.head)
+        }
       }
 
-      private given JsonValueCodec[Array[User]] = JsonCodecMaker.make(CodecMakerConfig.withDecodingOnly(true))
+      private given JsonValueCodec[List[User]] = JsonCodecMaker.make(CodecMakerConfig.withDecodingOnly(true))
     }
 
     private given JsonValueCodec[List[Post]] = JsonCodecMaker.make(CodecMakerConfig.withDecodingOnly(true))
