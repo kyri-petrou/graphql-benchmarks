67f9d8011098ee5c25f57db98f7273f14d154cea
Kyri Petrou <kyri.petrou@outlook.com>
2024-12-27 10:40:16 +0200

    Remove setup of Caliban

--- setup.sh
+++ setup.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
-# For gqlgen:
-cd graphql/gqlgen
-go build -o main main.go
-cd ../../
-# For apollo server:
-cd graphql/apollo_server
-npm i
-cd ../../
-
-# For netflix dgs
-cd graphql/netflix_dgs
-./gradlew build
-cd ../../
-
-# For tailcall:
-cd graphql/tailcall
-npm install
-cd ../../
+## For gqlgen:
+#cd graphql/gqlgen
+#go build -o main main.go
+#cd ../../
+## For apollo server:
+#cd graphql/apollo_server
+#npm i
+#cd ../../
+#
+## For netflix dgs
+#cd graphql/netflix_dgs
+#./gradlew build
+#cd ../../
+#
+## For tailcall:
+#cd graphql/tailcall
+#npm install
+#cd ../../
 
 # For caliban
 cd graphql/caliban
 ./sbt compile
 cd ../../
 
-# For async-graphql
-./graphql/async_graphql/build.sh
-
-# For hasura
-cd graphql/hasura
-npm install
-cd ../../
-
-# For graphql_jit
-cd graphql/graphql_jit
-npm install
-cd ../../
+## For async-graphql
+#./graphql/async_graphql/build.sh
+#
+## For hasura
+#cd graphql/hasura
+#npm install
+#cd ../../
+#
+## For graphql_jit
+#cd graphql/graphql_jit
+#npm install
+#cd ../../
